f4db44228c09a70e3a0495f4028184f66e2ba56e
jeshua123 <jeshuaestevez1@gmail.com>
2025-08-23 07:02:28 -0400

    first commit

---
+++ cien_ejercicios.js
@@ -0,0 +1,28 @@
+//100 Ejercicios de JavaScript para Consolidar Fundamentos
+//Esta colección de ejercicios está diseñada para reforzar todos los conceptos aprendidos en
+//la lección de JavaScript, incrementando la dificultad de forma progresiva.Variables y Tipos de Datos (1–10)
+
+
+//1. Declara una variable nombre con tu nombre y muéstrala en consola.
+
+const nombre="Jeshua"
+
+console.log(nombre)
+//2. Declara una constante PI con el valor 3.1416.
+
+
+//3. Cambia el valor de una variable edad y muestra ambos valores.
+
+//4. Declara variables de tipo string, number, boolean y muéstralas.
+
+//5. Usa typeof para mostrar el tipo de cada variable anterior.
+
+//6. Declara un array con 3 colores y accede al segundo elemento.
+
+//7. Declara un objeto persona con nombre, edad y ciudad.
+
+//8. Cambia la propiedad edad del objeto persona y muéstrala.
+
+//9. Declara una variable undefined y otra null y muestra sus tipos.
+
+//10. Crea una constante con un array de números y muéstralo en consola
