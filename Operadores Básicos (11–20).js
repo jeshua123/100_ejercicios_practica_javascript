ae698f6c5a49bb13494e0373b40981b01304b270
jeshua123 <jeshuaestevez1@gmail.com>
2025-08-26 01:41:07 -0400

    update 26-08-2025

---
+++ Operadores Básicos (11–20).js
@@ -0,0 +1,43 @@
+//Operadores Básicos (11–20)
+
+//11. Suma dos números y guarda el resultado en una variable.
+
+let num1=27
+let num2=72
+let suma=num1+num2
+
+//12. Resta dos números y muéstralo en consola.
+
+let resta=75-10
+console.log(resta)
+
+//13. Multiplica dos variables y guarda el resultado.
+
+let Multiplicación=69*81
+
+//14. Divide dos números y muestra el resultado.
+
+let division=56/41
+console.log(division)
+
+//15. Calcula el módulo de dos números.
+
+
+let numero1=56
+let numero2=7
+let mod=numero1%numero2
+
+//16. Usa == y === para comparar 5 y '5'.
+
+
+let comparación = 5=='5'
+let comparación2 = 5==='5'
+
+//17. Usa != y !== para comparar dos valores distintos.
+
+//18. Usa > para verificar si un número es mayor que otro.
+
+//19. Usa && para validar que dos condiciones sean verdaderas.
+
+//20. Usa || para verificar si una de dos condiciones es verdadera.
+
